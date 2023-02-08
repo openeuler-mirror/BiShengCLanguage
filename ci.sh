32691a89663919f7c198bfd91ed0be54277e3ec5
weixin93 <linweixin1@huawei.com>
2023-02-08 10:15:03 +0000

    update ci.sh.
    Signed-off-by: weixin93 <linweixin1@huawei.com>

--- ci.sh
+++ ci.sh
@@ -56,7 +56,7 @@ function update_submodule() {
 }
 
 function install_tools() {
-  sudo yum -y install python3 cmake git g++ dkms dpkg rsync
+  sudo yum -y install python3 cmake git g++ dkms dpkg rsync glibc-devel glibc
   sudo ln -sf /usr/lib/dkms/lsb_release /usr/bin/lsb_release
   sudo ln -sf ${ROOT_DIR}/libtinfo.so.5.9 /lib64/libtinfo.so.5
 }
