231b7886d5a75de3cf9ca39128a506e0bd633760
蒋群超 <jiangqunchao@huawei.com>
2023-02-08 07:57:14 +0000

    update ci.sh.
    Signed-off-by: 蒋群超 <jiangqunchao@huawei.com>

--- ci.sh
+++ ci.sh
@@ -111,6 +111,7 @@ function get_owner_info() {
 
 function start_ci_test() {
   cd ${SUB_OAC_DIR}
+  ll ${SUB_OAC_DIR}/output/aarch64-clang-release/bin/
   source build/envsetup.sh arm release
   mm c_test
 }
