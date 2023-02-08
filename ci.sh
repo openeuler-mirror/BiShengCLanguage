823ef5a12977eb2d078b11884c7784984748156a
weixin_93 <linweixin1@huawei.com>
2023-02-08 16:10:22 +0800

    upload libtinfo.so.5

--- ci.sh
+++ ci.sh
@@ -58,7 +58,7 @@ function update_submodule() {
 function install_tools() {
   sudo yum -y install python3 cmake git g++ dkms dpkg rsync
   sudo ln -sf /usr/lib/dkms/lsb_release /usr/bin/lsb_release
-  sudo ln -sf /lib64/libtinfo.so.6 /lib64/libtinfo.so.5
+  sudo ln -sf ${ROOT_DIR}/libtinfo.so.5.9 /lib64/libtinfo.so.5
 }
 
 function get_branch_code() {
