703b5a62212c4f11b6cbda4b90301f1290ae1a87
蒋群超 <jiangqunchao@huawei.com>
2023-02-09 01:25:02 +0000

    update ci.sh.
    Signed-off-by: 蒋群超 <jiangqunchao@huawei.com>

--- ci.sh
+++ ci.sh
@@ -59,12 +59,13 @@ function install_tools() {
   sudo yum -y install python3 cmake git g++ dkms dpkg rsync glibc-devel glibc
   sudo ln -sf /usr/lib/dkms/lsb_release /usr/bin/lsb_release
   sudo ln -sf ${ROOT_DIR}/libtinfo.so.5.9 /lib64/libtinfo.so.5
+  ls /etc/yum.repos.d/
   sudo mv /etc/yum.repos.d/openEuler.repo /etc/yum.repos.d/openEuler.repo.backup
   sudo cp -rf ${ROOT_DIR}/script/Centos-Base.repo /etc/yum.repos.d/
-  yum clean all
-  yum makecache
-  yum -y install glibc-devel.i686
-  pip install paramiko
+  sudo yum clean all
+  sudo yum makecache
+  sudo yum -y install glibc-devel.i686
+  sudo pip install paramiko
   sudo rm -rf /etc/yum.repos.d/Centos-Base.repo
   sudo mv /etc/yum.repos.d/openEuler.repo.backup /etc/yum.repos.d/openEuler.repo
 }
